b1d989c2da0613b611dea5ffaad67277177bd8c6
bernardocdm <bcdmercado@sga.pucminas.br>
2025-04-22 19:46:26 -0300

    Initial commit

---
+++ src/assets/App2.jsx
@@ -0,0 +1,68 @@
+function App() {
+  return (
+    <h1>Olá, Mundo!</h1>
+  )
+}
+
+export default App
+
+
+// rafce
+
+// Arrow Function
+// const App2 = () => <h1>Olá, Mundo!</h1>;
+
+// Nomeação de Componente
+// PascalCase
+
+// Nomeação de Variavel, Função...
+// camelCase
+
+// export default, posso importar com qualquer nome e sem chaves
+// export "sem default", só posso importar entre chaves e com o nome exportado
+
+// Self closing tag
+// <Header></Header>
+// <Header/>
+
+// element.style {
+//   background-color: antiquewhite;
+//   padding: 20px;
+//   margin: 20px;
+//   border: solid 5px black;
+//   /* box-sizing: content-box; */
+//   width: 500px;
+// }
+
+// Nomeação de classes em CSS
+// Metodologia BEM
+// Blocks
+// Elements
+// Modifiers
+// bloco__elemento--modificador
+// header
+// header__link
+// header__link--small
+// item-list__header
+// Nomes compostos são separados por -
+
+// Tag vazia em React se chama Fragment ou Fragmento
+
+//.fill preenche com alguma coisa indefinida
+// .fill / .map
+
+
+{/* <div className="item-list__container">
+{Array(items).fill()
+.map((currentValue, index) => (
+  <SingleItem key= {`${title}-${index}`} />
+))} */}
+
+
+// id = {currobj.id}
+// name = {currobj.name}
+// image = {currobj.image}
+// banner = {currobj.banner}
+
+
+// Ultimo visto foi o conteudo de routes
